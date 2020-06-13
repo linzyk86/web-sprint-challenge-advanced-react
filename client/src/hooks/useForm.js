109218c8af756cfef375af5caf575d05bee1a93a
Lindsay W <lwhite5314@gmail.com>
2020-06-12 20:25:57 -0400

    ...

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,26 @@
 // write your custom hook here to control your checkout form
+export const useForm = ( initialValues) => {
+    const[setValues, values] = [];
+
+     const handleChanges = e => {
+    console.log(e.target.name);
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+    const handleSubmit = e => {
+        if (e) e.preventDefault();
+        
+    };
+
+    const clearForm = e => {
+    e.preventDefault();
+    setValues(initialValues);
+  };
+    
+
+    return [values, clearForm, handleSubmit, handleChanges];
+
+}
